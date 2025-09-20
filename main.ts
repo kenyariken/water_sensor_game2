2d72a9b98afd66fae5945a12e0245ae1eb2c106e
Ken <161325958+kenyariken@users.noreply.github.com>
2025-09-20 21:36:40 +0900

    ゲームが偽になった後も、最後のタイムを表示し続けます。こうすると最初は0がLEDに表示されます。

--- main.ts
+++ main.ts
@@ -5,16 +5,16 @@ input.onButtonPressed(Button.A, function () {
     time = 0
     playing = true
 })
-// ずっとカウント
 basic.forever(function () {
     if (playing) {
         time += 1
         basic.showNumber(time)
-        // 1秒ごとに数える
         basic.pause(1000)
+    } else {
+        // playingがfalseならタイム値を表示し続ける
+        basic.showNumber(time)
     }
     if (playing && pins.analogReadPin(AnalogPin.P0) > 300) {
-        // 水がたまったら止める
         playing = false
     }
 })
